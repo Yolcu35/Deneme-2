9a71e42174975e53072e42328ae4a6ea5d1c2487
Yolcu35 <katuran70@gmail.com>
2026-08-14 19:22:32 +0300

    Update SetFilmizlePlugin.kt

--- SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizlePlugin.kt
+++ SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizlePlugin.kt
@@ -1,11 +1,12 @@
 package com.aykut.setfilmizle
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class SetFilmizlePlugin : Plugin() {
+
     override fun load(context: Context) {
         registerMainAPI(SetFilmizleProvider())
     }
