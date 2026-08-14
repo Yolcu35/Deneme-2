a6656df35bc56ff4d48fc982a5412fc9a4e0b687
Yolcu35 <katuran70@gmail.com>
2026-08-14 13:12:14 +0300

    Update build.gradle.kts

--- SetFilmizle/build.gradle.kts
+++ SetFilmizle/build.gradle.kts
@@ -14,7 +14,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 35
     }
 
     compileOptions {
@@ -22,8 +21,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        }
     }
 }
 
