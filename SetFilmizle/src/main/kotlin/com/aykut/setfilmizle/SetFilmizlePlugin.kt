9251c8519e7c9322aaa9ee8bcae36fb17eaa55c1
Yolcu35 <katuran70@gmail.com>
2026-08-14 13:01:55 +0300

    Update SetFilmizlePlugin.kt

--- SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizlePlugin.kt
+++ SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizlePlugin.kt
@@ -1,8 +1,8 @@
 package com.aykut.setfilmizle
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
 class SetFilmizlePlugin : Plugin() {
